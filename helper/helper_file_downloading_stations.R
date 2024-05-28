423e6d40399bd745f9c095a19b611f9a135cca6e
Kate Saunders <kate.saunders@monash.edu>
2024-05-29 06:03:15 +1000

    Making variable names clearer

--- helper/helper_file_downloading_stations.R
+++ helper/helper_file_downloading_stations.R
@@ -3,52 +3,35 @@ library(tidyverse)
 library(oz)
 
 aus_code  = "ASN"
-min_years = 50
 var = c("PRCP", "DAPR", "DWPR", "MDPR")
-delta = 50
-east_coast_longitude = 150
 
-all_stations <- ghcnd_stations()
+all_stations_meta <- ghcnd_stations()
 
 # Australian stations
-aus_stations <- all_stations %>%
+aus_stations_meta <- all_stations_meta %>%
   filter(element == "PRCP") %>%
   filter(str_detect(id, aus_code)) %>%
   filter(longitude > 0) # removes ocean
 
 # Southeast Queensland Stations
-seq_stations <- all_stations |>
+seq_stations_meta <- all_stations_meta |>
   filter(element == "PRCP") |>
   filter(str_detect(id, aus_code)) |>
   filter(latitude < -25 & latitude > -28.5) |>
   filter(longitude > 151 & longitude < 153.6)
 
 seq_station_plot <- ggplot()  +
-  geom_point(data  = aus_stations,
+  geom_point(data  = aus_stations_meta,
              aes(x= longitude,  y = latitude),
              shape = 21, size = 0.2) +
-  geom_point(data =  seq_stations,
-             aes(x= longitude,  y = latitude),
-             shape = 21, fill = "red") +
-  coord_fixed()
-
-# Reduced Figure Stations
-fig_stations <- seq_stations |>
-  filter(latitude < -27.4 & latitude > -27.55) |>
-  filter(longitude > 152.9 & longitude < 153.1)
-
-fig_station_plot <- ggplot()  +
-  geom_point(data  = seq_stations,
-             aes(x= longitude,  y = latitude),
-             shape = 21, size = 0.2) +
-  geom_point(data =  fig_stations,
+  geom_point(data =  seq_stations_meta,
              aes(x= longitude,  y = latitude),
              shape = 21, fill = "red") +
   coord_fixed()
 
 # Data download
 # ~12 minutes for 435 stations
-monitors <- seq_stations$id
+monitors <- seq_stations_meta$id
 time1 <- Sys.time()
 reduced_station_data <- meteo_pull_monitors(monitors = monitors,
                                             keep_flags =  TRUE,
